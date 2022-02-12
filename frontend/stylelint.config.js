5c42b96b3ffb36a7342774cfc17a399fa96b977e
Alex Gustafsson <alex-gustafsson@live.se>
2022-02-12 11:57:19 +0100

    Add prettier

--- frontend/stylelint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "stylelint-config-standard",
-  rules: {
-    "color-hex-case": "upper",
-    "declaration-empty-line-before": null,
-    "comment-empty-line-before": null,
-    "declaration-bang-space-before": null,
-  },
-};
